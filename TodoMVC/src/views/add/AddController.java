64ef32547121197502de0b1d5d32fb7a24e2cb2e
TRMO <tromortensen@hotmail.com>
2020-11-16 20:56:37 +0100

    Can now add todo

--- TodoMVC/src/views/add/AddController.java
+++ TodoMVC/src/views/add/AddController.java
@@ -2,14 +2,34 @@ package views.add;
 
 import core.VMFactory;
 import core.ViewHandler;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import views.ViewController;
 
 public class AddController implements ViewController {
 
+    public TextField userIdField;
+    public TextField bodyField;
+    public Label validationLabel;
 
+    private ViewHandler vh;
+    private AddVM addVM;
 
     @Override
     public void init(ViewHandler viewHandler, VMFactory vmFactory, Object argForView) {
+        vh = viewHandler;
+        addVM = vmFactory.getAddVM();
+        userIdField.textProperty().bindBidirectional(addVM.userIdProperty());
+        bodyField.textProperty().bindBidirectional(addVM.bodyProperty());
+        validationLabel.textProperty().bind(addVM.validationProperty());
+    }
 
+    public void onCreate() {
+        addVM.create();
+    }
+
+    public void onBack() {
+        addVM.clear();
+        vh.openView("display");
     }
 }
--- TodoMVC/src/views/add/AddVM.java
+++ TodoMVC/src/views/add/AddVM.java
@@ -1,9 +1,54 @@
 package views.add;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import models.Todo;
 import services.TodoService;
 
 public class AddVM {
-    public AddVM(TodoService todoService) {
+    private final TodoService todoService;
+    private final StringProperty userIdProperty = new SimpleStringProperty();
+    private final StringProperty bodyProperty = new SimpleStringProperty();
+    private final StringProperty validationProperty = new SimpleStringProperty();
 
+    public AddVM(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public void create() {
+        Integer userId = null;
+        try {
+            userId = Integer.parseInt(userIdProperty.get());
+        } catch (NumberFormatException e) {
+            validationProperty.set("User id must be number, larger than 0");
+            return;
+        }
+        String body = bodyProperty.get();
+        if (body == null || "".equals(body)) {
+            validationProperty.set("Body cannot be empty");
+            return;
+        }
+        Todo todo = new Todo(userId, body);
+        todoService.create(todo);
+        clear();
+        validationProperty.set("Created");
+    }
+
+    public void clear() {
+        userIdProperty.set("");
+        bodyProperty.set("");
+        validationProperty.set("");
+    }
+
+    public StringProperty userIdProperty() {
+        return userIdProperty;
+    }
+
+    public StringProperty bodyProperty() {
+        return bodyProperty;
+    }
+
+    public StringProperty validationProperty() {
+        return validationProperty;
     }
 }
